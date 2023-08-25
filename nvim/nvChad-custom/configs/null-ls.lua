302df98da36a39c54f45e46b5be4864372587eec
Alexander Hirdman <88927248+ahirdman@users.noreply.github.com>
2023-08-26 01:35:49 +0200

    Update null_ls builtin formatters

--- nvim/nvChad-custom/configs/null-ls.lua
+++ nvim/nvChad-custom/configs/null-ls.lua
@@ -1,25 +1,25 @@
 local present, null_ls = pcall(require, "null-ls")
 
 if not present then
-  return
+	return
 end
 
 local b = null_ls.builtins
 
 local sources = {
 
-  -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "yaml" } }, -- so prettier works only on these filetypes
+	-- webdev stuff
+	b.formatting.deno_fmt.with({ filetypes = { "javascript", "javascriptreact", "json", "jsonc", "markdown", "typescript", "typescriptreact" } }),
+	b.formatting.prettier.with({ filetypes = { "html", "css", "scss", "yaml", "handlebars" } }),
 
-  -- Lua
-  b.formatting.stylua,
+	-- Lua
+	b.formatting.stylua,
 
-  -- cpp
-  b.formatting.clang_format,
+	-- cpp
+	b.formatting.clang_format,
 }
 
-null_ls.setup {
-  debug = true,
-  sources = sources,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+})
