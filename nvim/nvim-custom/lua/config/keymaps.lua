574e58b008c014f27dcf486d6e3d4cbe563584c1
Alexander Hirdman <88927248+ahirdman@users.noreply.github.com>
2024-01-14 00:55:14 +0100

    Trouble and which key - WIP

--- nvim/nvim-custom/lua/config/keymaps.lua
+++ nvim/nvim-custom/lua/config/keymaps.lua
@@ -1,40 +1,30 @@
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
--- navigate within insert mode
-vim.keymap.set({ "i" }, "<C-h>", "<Left>")
-
 -- clear highlight
 vim.keymap.set({ "n" }, "<Esc>", ":noh <CR>")
 
-vim.keymap.set({ "i" }, "<C-l>", "<Right>")
-vim.keymap.set({ "i" }, "<C-j>", "<Down>")
-vim.keymap.set({ "i" }, "<C-k>", "<Up>")
+-- switch between windows
+vim.keymap.set({ "n" }, "<C-h>", "<C-w>h")                       -- Move left
+vim.keymap.set({ "n" }, "<C-l>", "<C-w>l")                       -- Move Right
+vim.keymap.set({ "n" }, "<C-l>", "<C-w>j")                       -- Move Down
+vim.keymap.set({ "n" }, "<C-l>", "<C-w>k")                       -- Move Up
+
+vim.keymap.set({ "n" }, "<C-h>", "<cmd> TmuxNavigateLeft <CR>")  -- Move left
+vim.keymap.set({ "n" }, "<C-l>", "<cmd> TmuxNavigateRight <CR>") -- Move Right
+vim.keymap.set({ "n" }, "<C-l>", "<cmd> TmuxNavigateDown <CR>")  -- Move Down
+vim.keymap.set({ "n" }, "<C-l>", "<cmd> TmuxNavigateUp <CR>")    -- Move Up
+
+-- save
+vim.keymap.set({ "n" }, "<C-s>", "<cmd> w <CR>")
 
 -- move line up/down in visual mode
 vim.keymap.set({ "v" }, "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set({ "v" }, "K", ":m '<-2<CR>gv=gv")
 
--- switch between windows
-vim.keymap.set({ "n" }, "<C-h>", "<C-w>h") -- Move left
-vim.keymap.set({ "n" }, "<C-l>", "<C-w>l") -- Move Right
-vim.keymap.set({ "n" }, "<C-l>", "<C-w>j") -- Move Down
-vim.keymap.set({ "n" }, "<C-l>", "<C-w>k") -- Move Up
 
-vim.keymap.set({ "n" }, "<C-h>", "<cmd> TmuxNavigateLeft <CR>") -- Move left
-vim.keymap.set({ "n" }, "<C-l>", "<cmd> TmuxNavigateRight <CR>") -- Move Right
-vim.keymap.set({ "n" }, "<C-l>", "<cmd> TmuxNavigateDown <CR>") -- Move Down
-vim.keymap.set({ "n" }, "<C-l>", "<cmd> TmuxNavigateUp <CR>") -- Move Up
+-- navigate within insert mode
+vim.keymap.set({ "i" }, "<C-h>", "<Left>")
 
--- save
-vim.keymap.set({ "n" }, "<C-s>", "<cmd> w <CR>")
-
--- format
-vim.keymap.set({ "n" }, "<Space>fm", function()
-	vim.lsp.buf.format({ async = true })
-end)
-
--- Diagnostic keymaps
--- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
--- vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-vim.keymap.set("n", "<leader>f", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+vim.keymap.set({ "i" }, "<C-l>", "<Right>")
+vim.keymap.set({ "i" }, "<C-j>", "<Down>")
+vim.keymap.set({ "i" }, "<C-k>", "<Up>")
--- nvim/nvim-custom/lua/plugins/telescope.lua
+++ nvim/nvim-custom/lua/plugins/telescope.lua
@@ -67,21 +67,6 @@ return {
 		end
 
 		vim.api.nvim_create_user_command("LiveGrepGitRoot", live_grep_git_root, {})
-		vim.keymap.set("n", "<leader>sG", ":LiveGrepGitRoot<cr>", { desc = "[S]earch by [G]rep on Git Root" })
-
-		-- See `:help telescope.builtin`
-		vim.keymap.set(
-			"n",
-			"<leader>?",
-			require("telescope.builtin").oldfiles,
-			{ desc = "[?] Find recently opened files" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader><space>",
-			require("telescope.builtin").buffers,
-			{ desc = "[ ] Find existing buffers" }
-		)
 
 		vim.keymap.set("n", "<leader>/", function()
 			-- You can pass additional configuration to telescope to change theme, layout, etc.
@@ -102,13 +87,5 @@ return {
 
 		vim.keymap.set("n", "<leader>s/", telescope_live_grep_open_files, { desc = "[S]earch [/] in Open Files" })
 
-		vim.keymap.set("n", "<leader>ss", telescope_builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		vim.keymap.set("n", "<leader>gf", telescope_builtin.git_files, { desc = "Search [G]it [F]iles" })
-		vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, { desc = "[F]ind [F]iles" })
-		vim.keymap.set("n", "<leader>sh", telescope_builtin.help_tags, { desc = "[S]earch [H]elp" })
-		vim.keymap.set("n", "<leader>sw", telescope_builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sg", telescope_builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>sd", telescope_builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-		vim.keymap.set("n", "<leader>sr", telescope_builtin.resume, { desc = "[S]earch [R]esume" })
 	end,
 }
