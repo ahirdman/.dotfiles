b3e87d5de816cd957398a26ce370cf5716cd4d0c
Alexander Hirdman <88927248+ahirdman@users.noreply.github.com>
2024-04-17 22:28:55 +0200

    Add sqls lsp and database setup

--- nvim/lua/config/lsp/servers.lua
+++ nvim/lua/config/lsp/servers.lua
@@ -17,4 +17,17 @@ return {
       diagnostics = { disable = { 'missing-fields' } },
     },
   },
+  sqls = {
+    settings = {
+      sqls = {
+        connections = {
+          {
+            driver = 'postgresql',
+            dataSourceName =
+            'host=127.0.0.1 port=15432 user=postgres password=mysecretpassword1234 dbname=dvdrental sslmode=disable',
+          },
+        },
+      },
+    },
+  }
 }
