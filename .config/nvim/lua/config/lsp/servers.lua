082e77d370155fa989d49b2c90c871bd09df7e43
Alexander Hirdman <126080861+AlexHird@users.noreply.github.com>
2024-10-04 12:51:58 +0200

    Add harper LSP

--- .config/nvim/lua/config/lsp/servers.lua
+++ .config/nvim/lua/config/lsp/servers.lua
@@ -1,6 +1,6 @@
 ---@type lspconfig.options
 local servers = {
-  -- Work specifik LSP servers
+  -- Work specific LSP servers
   eslint = {
     settings = {
       codeAction = {
@@ -44,11 +44,7 @@ local servers = {
       }
     },
   },
-  biome = {
-    settings = {
-      enable = false
-    }
-  },
+  biome = {},
   tailwindcss = {
     tailwindCSS = {
       experimental = {
@@ -57,6 +53,29 @@ local servers = {
           { "cx\\(([^)]*)\\)",  "(?:'|\"|`)([^']*)(?:'|\"|`)" }
         },
       },
+    }
+  },
+  harper_ls = {
+    ["harper-ls"] = {
+
+      linters = {
+        spell_check = true,
+        spelled_numbers = false,
+        an_a = true,
+        sentence_capitalization = false,
+        unclosed_quotes = true,
+        wrong_quotes = false,
+        long_sentences = true,
+        repeated_words = true,
+        spaces = true,
+        matcher = true,
+        correct_number_suffix = true,
+        number_suffix_capitalization = true,
+        multiple_sequential_pronouns = true,
+        linking_verbs = false,
+        avoid_curses = true,
+        terminating_conjunctions = true
+      }
     }
   },
   pyright = {},
