efbaf08fa031cf4af82f08ab4c69e86f0d7c68d7
Alexander Hirdman <126080861+AlexHird@users.noreply.github.com>
2024-10-04 20:03:50 +0200

    Minor fixes

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -16,3 +16,21 @@ autocmd("TextYankPost", {
 	group = highlight_group,
 	pattern = "*",
 })
+
+autocmd({ "InsertLeave", "WinEnter" }, {
+	callback = function()
+		if vim.w.auto_cursorline then
+			vim.wo.cursorline = true
+			vim.w.auto_cursorline = nil
+		end
+	end,
+})
+
+autocmd({ "InsertEnter", "WinLeave" }, {
+	callback = function()
+		if vim.wo.cursorline then
+			vim.w.auto_cursorline = true
+			vim.wo.cursorline = false
+		end
+	end,
+})
