eba25ca8bddb07bddd82de58c69e469f750f4215
Alexander Hirdman <126080861+AlexHird@users.noreply.github.com>
2024-10-09 22:31:17 +0200

    Remove harper

--- .config/nvim/lua/config/lsp/servers.lua
+++ .config/nvim/lua/config/lsp/servers.lua
@@ -53,29 +53,6 @@ local servers = {
 			},
 		},
 	},
-	harper_ls = {
-		["harper-ls"] = {
-
-			linters = {
-				spell_check = true,
-				spelled_numbers = false,
-				an_a = true,
-				sentence_capitalization = false,
-				unclosed_quotes = true,
-				wrong_quotes = false,
-				long_sentences = true,
-				repeated_words = true,
-				spaces = false,
-				matcher = true,
-				correct_number_suffix = true,
-				number_suffix_capitalization = true,
-				multiple_sequential_pronouns = true,
-				linking_verbs = false,
-				avoid_curses = true,
-				terminating_conjunctions = true,
-			},
-		},
-	},
 	pyright = {},
 	taplo = {},
 	ts_ls = {
