3da9f8e904ee5b699b1665b967335aac24bfb061
Alexander Hirdman <126080861+AlexHird@users.noreply.github.com>
2024-11-12 14:37:17 +0100

    Fix "g" function

--- nix-darwin/home/zsh.nix
+++ nix-darwin/home/zsh.nix
@@ -6,30 +6,25 @@
     syntaxHighlighting.enable = true;
 
     shellAliases = {
-      v = "nvim";
-      switch = "darwin-rebuild switch --flake ~/.dotfiles/nix-darwin#work";
-
       ".." = "cd ..";
       "..." = "cd ../..";
-
-      cl = "clear";
-
+      g = "g() { [ $# -gt 0 ] && git $@ || git status -s; };g";
       ls = "eza -la --icons --git --group-directories-first";
       lt = "eza --tree --level=2 --icons --all --ignore-glob='node_modules' ";
-
+      v = "nvim";
+      switch = "darwin-rebuild switch --flake ~/.dotfiles/nix-darwin#work";
+      cl = "clear";
+      cat = "bat";
+      ts = "tmuxPickSession";
+      gb = "switchBranch";
+      gc = "clone";
+      gp = "git pull";
       glt = "git log --oneline --decorate --graph --all";
       glta = "git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --all";
       coa = "git add -A && git commit -m";
-      gc = "clone";
-      gb = "switchBranch";
-      gp = "git pull";
       pr = "gh pr list | cut -f1,2 | gum choose | cut -f1 | xargs gh pr checkout";
       gclean = "git branch | cut -c 3- | gum choose --no-limit | xargs git branch -D";
       gpick = "git log --oneline | gum filter | cut -d' ' -f1 # | copy";
-
-      cat = "bat";
-
-      ts = "tmuxPickSession";
     };
 
     history = {
@@ -42,59 +37,37 @@
     initExtra = ''
       function cd() {
         builtin cd "$@"
-
         local nvmrc_file=".nvmrc"
-
         if [[ -f "$PWD/$nvmrc_file" ]]; then
           local desired_version=$(cat "$PWD/$nvmrc_file")
           local current_version=$(node -v)
-
           if [[ "$current_version" != "$desired_version" ]]; then
             n auto
           fi
         fi
       }
-
-      function g() {
-        if [ $# -gt 0 ]; then
-          git "$@"
-        else
-          git status -s
-        fi
-      }
-
       function switchBranch() {
         BRANCH=$(git branch -v | gum choose |  awk "{print $1}")
-
         git switch $BRANCH
       }
-
       function clone() {
         echo "# Clone Repository with SSH profile: \n" | gum format
         echo ""
-
         MODE=$(gum choose  "Work" "Personal")
-
         GIT_URL=$(gum input --placeholder "Repositry url")
-
         REPO=$\{GIT_URL#*:}
-
         PREFIX=""
-
         if [ "$MODE" = "Work" ]; then
           PREFIX="workgit"
         else
           PREFIX="personalgit"
         fi
-
         gum confirm "Clone repo?" && git clone --bare $PREFIX:$REPO
       }
-
       function tmuxPickSession() {
         SESSION=$(tmux list-sessions -F \#S | gum filter --placeholder "Pick session...");
         tmux switch-client -t $SESSION || tmux attach -t $SESSION
       }
-
       eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/base.toml)"
       eval "$(direnv hook zsh)"
       eval "$(zoxide init zsh)"
